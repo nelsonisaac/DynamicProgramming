24509fa53accde27d5a2e660a063091c1ed4cbce
DevadasuNelson Isaac <devadasunelson.10715196@ltimindtree.com>
2023-12-08 12:49:26 +0530

    LCS problems

--- src/LongestCommonSubSeq/LCSubString.java
+++ src/LongestCommonSubSeq/LCSubString.java
@@ -10,18 +10,20 @@ public class LCSubString {
         for(int j=0;j<=m;j++){
             t[0][j] = 0;
         }
+
+        // We only note the values which have same character, else we keep 0 as we dont want any discontinuity, our string should be continuous 
         int len = 0;
         int row =0, col = 0;
         for(int i=1;i<=l;i++){
             for(int j=1;j<=m;j++){
                 if(a.charAt(i-1) == b.charAt(j-1)){
                     t[i][j] = 1+t[i-1][j-1];
+                    // We store the max value in len, and note the i,j values to print the string from this position in the matrix
                     if(len<t[i][j]){
                         len = t[i][j];
                         row = i;
                         col = j;
                     }
-                    max = Math.max(max,t[i][j]);
                 }else{
                     t[i][j] = 0;
                 }
@@ -34,22 +36,16 @@ public class LCSubString {
             System.out.println(" ");
         }
         
+        // printing the substring , where we recorded the positions of the i,j.
+        // from this position we go backward direction until we get 0 which prints our required string
         String s = "";
-        int i=l,j=m;
         while(t[row][col] != 0){
             if(a.charAt(row-1) == b.charAt(col-1)){
                 s=s+a.charAt(row-1);
                 row--;
                 col--;
             }
-            // }else{
-            //     if(t[i-1][j]>t[i][j]){
-            //         i--;
-            //     }else{
-            //         j--;
-            //     }
-            //     s="";
-            // }
+
         }
         String s2 = new StringBuilder(s).reverse().toString();
         System.out.println(s2);
---
+++ src/LongestCommonSubSeq/NoofReapeatingAinB.java
@@ -0,0 +1,31 @@
+package LongestCommonSubSeq;
+
+public class NoofReapeatingAinB {
+    private static int ReapeatingAinB(String a, String b, int l, int m){
+        if((l==0 && m==0) || m == 0){
+            return 1;
+        }
+        if(l==0){
+            return 0;
+        }
+        // We iterate through each character, from right to left.
+        // If characters are equal then we get 2 cases (l-1,m-1) and (l-1,m) so that the character 'm' subtracted in (l-1,m-1) is also included in the next iteration in (l-1,m) because l might contain that eliminated character repeatedly so to include that we take 2 cases
+        // We start with l>m, but as iteration progresses, we subtract l-1,m-1 only when equal, so evetually it reaches l==0&&m==0 , so return 1.
+        // the same aaplies to m=0, that means all the characters in 'm' are iterated since we decrease 'm' only if there a equal character in 'l'. therfore return 1 when m==0;
+        // when l=0, it means eventually nothing matches (not equal to case) so return 0.
+        if(a.charAt(l-1) == b.charAt(m-1)){
+            return (ReapeatingAinB(a, b, l-1, m-1) + ReapeatingAinB(a, b, l-1, m));
+        }else{
+            return ReapeatingAinB(a, b, l-1, m);
+        }
+    }
+
+    public static void main(String[] args){
+        String a = "geeksforgeeks";
+        String b = "gks";
+        int l = a.length();
+        int m = b.length();
+
+        System.out.println(ReapeatingAinB(a, b, l, m));
+    }
+}
